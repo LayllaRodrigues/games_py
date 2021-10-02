58352e6d194c5f38a7b517f6773f7372cd3a80ed
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-10-02 15:01:01 -0300

    incluindo for range

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,7 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
