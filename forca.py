e48ea5714ad4a95cf39d83727634161505853039
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-10-03 21:42:23 -0300

    adc bool no retorno

--- forca.py
+++ forca.py
@@ -1,5 +1,18 @@
-print("*********************************")
-print("***Bem vindo ao jogo da Forca!***")
-print("*********************************")
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-print("Fim do jogo")
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+
+if(__name__ == "__main__"):
+    jogar()
